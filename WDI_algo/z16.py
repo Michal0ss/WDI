92059cef1ac9aac0d887f6e0eee5bafcf8d9825e
Michal0ss <michal.bialas.pol@gmail.com>
2024-10-28 00:02:59 +0100

    added 2 updates

---
+++ WDI_algo/z16.py
@@ -0,0 +1,23 @@
+#  Proszę napisać program wyznaczający najmniejszą wspólną wielokrotność 3 zadanych liczb naturalnych.
+#NWW(a,b)= ∣a⋅b∣ / NWD(a,b)
+
+
+
+def nwd(a,b):
+
+    while b!=0:
+        temp =b
+        b = a%b
+        a = temp
+    return a
+
+def nww(a,b):
+    return abs(a*b) // nwd(a,b)
+
+def trd_nww(a,b,c):
+    return nww(nww(a,b), c)
+
+x=int(input("a "))
+y=int(input("b "))
+z=int(input("c "))
+print(f"NWW liczb {x}, {y} i {z} to: {trd_nww(x, y, z)}")
